4daa64cc91610cf36fdbaf52d9a5ae177e0b04e8
Nikolas Koesling <nikolas@koesling.info>
2022-07-26 18:24:50 +0200

    increase version 1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,57 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char **) {
+int main(int argc, char **argv) {
     if (argc != 1) {
-        std::cerr << "usage: modbus_conv_float" << std::endl;
-        std::cerr << "  Converts float values to use them with stdin_to_modbus_shm." << std::endl;
-        std::cerr << "  If line starts with 'f:', the 'f:' is removed and the float value is converted to two hex "
-                     "registers."
-                  << std::endl;
-        std::cerr << "  All other lines are passed through." << std::endl;
-        std::cerr << std::endl;
-        std::cerr << "  example:" << std:: endl;
-        std::cerr << "    input : f:ao:0xcf:123.4" << std::endl;
-        std::cerr << "    output: ao:0xcf:0x42f6" << std::endl;
-        std::cerr << "            ao:0xd0:0xcccd" << std::endl;
-        std::cout << std::endl;
-        std::cout << std::endl;
-        std::cout << "MIT License:" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Copyright (c) 2021 Nikolas Koesling" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Permission is hereby granted, free of charge, to any person obtaining a copy" << std::endl;
-        std::cout << "of this software and associated documentation files (the \"Software\"), to deal" << std::endl;
-        std::cout << "in the Software without restriction, including without limitation the rights" << std::endl;
-        std::cout << "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" << std::endl;
-        std::cout << "copies of the Software, and to permit persons to whom the Software is" << std::endl;
-        std::cout << "furnished to do so, subject to the following conditions:" << std::endl;
-        std::cout << std::endl;
-        std::cout << "The above copyright notice and this permission notice shall be included in all" << std::endl;
-        std::cout << "copies or substantial portions of the Software." << std::endl;
-        std::cout << std::endl;
-        std::cout << "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" << std::endl;
-        std::cout << "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," << std::endl;
-        std::cout << "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" << std::endl;
-        std::cout << "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" << std::endl;
-        std::cout << "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," << std::endl;
-        std::cout << "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE" << std::endl;
-        std::cout << "SOFTWARE." << std::endl;
-                exit(EX_USAGE);
+        std::string arg(argv[1]);
+
+        if (arg == "-v" || arg == "--version") {
+            std::cout << "modbus_conv_float " << PROJECT_VERSION << std::endl;
+            exit(EX_OK);
+        }
+
+        const bool    ARG_HELP = arg == "--help" || arg == "-h";
+        std::ostream &out      = ARG_HELP ? std::cout : std::cerr;
+
+        out << "usage: modbus_conv_float [OPTION...]" << std::endl;
+        out << std::endl;
+        out << "    -h --help     print this message." << std::endl;
+        out << "    -v --version  print version information." << std::endl;
+        out << std::endl;
+        out << "  Converts float values to use them with stdin_to_modbus_shm." << std::endl;
+        out << "  If line starts with 'f:', the 'f:' is removed and the float value is converted to two hex "
+               "registers."
+            << std::endl;
+        out << "  All other lines are passed through." << std::endl;
+        out << std::endl;
+        out << "  example:" << std::endl;
+        out << "    input : f:ao:0xcf:123.4" << std::endl;
+        out << "    output: ao:0xcf:0x42f6" << std::endl;
+        out << "            ao:0xd0:0xcccd" << std::endl;
+        out << std::endl;
+        out << std::endl;
+        out << "MIT License:" << std::endl;
+        out << std::endl;
+        out << "Copyright (c) 2021-2022 Nikolas Koesling" << std::endl;
+        out << std::endl;
+        out << "Permission is hereby granted, free of charge, to any person obtaining a copy" << std::endl;
+        out << "of this software and associated documentation files (the \"Software\"), to deal" << std::endl;
+        out << "in the Software without restriction, including without limitation the rights" << std::endl;
+        out << "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" << std::endl;
+        out << "copies of the Software, and to permit persons to whom the Software is" << std::endl;
+        out << "furnished to do so, subject to the following conditions:" << std::endl;
+        out << std::endl;
+        out << "The above copyright notice and this permission notice shall be included in all" << std::endl;
+        out << "copies or substantial portions of the Software." << std::endl;
+        out << std::endl;
+        out << "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" << std::endl;
+        out << "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," << std::endl;
+        out << "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" << std::endl;
+        out << "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" << std::endl;
+        out << "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," << std::endl;
+        out << "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE" << std::endl;
+        out << "SOFTWARE." << std::endl;
+        exit(ARG_HELP ? EX_OK : EX_USAGE);
     }
 
     std::string line;
@@ -74,12 +88,12 @@ int main(int argc, char **) {
             }
 
             static_assert(sizeof(float) == 2 * sizeof(uint16_t));
-            void *         v  = &f;
-            const uint16_t lo = *reinterpret_cast<uint32_t *>(v) & 0xFFFF;
-            const uint16_t hi = (*reinterpret_cast<uint32_t *>(v)) >> 16 & 0xFFFF;
+            void          *v  = &f;
+            const uint16_t LO = static_cast<uint16_t>(*reinterpret_cast<uint32_t *>(v) & 0xFFFF);
+            const uint16_t HI = static_cast<uint16_t>((*reinterpret_cast<uint32_t *>(v)) >> 16 & 0xFFFF);
 
-            std::cout << split_line[0] << ":0x" << std::hex << addr << ":0x" << std::hex << hi << std::endl;
-            std::cout << split_line[0] << ":0x" << std::hex << addr + 1 << ":0x" << std::hex << lo << std::endl;
+            std::cout << split_line[0] << ":0x" << std::hex << addr << ":0x" << std::hex << HI << std::endl;
+            std::cout << split_line[0] << ":0x" << std::hex << addr + 1 << ":0x" << std::hex << LO << std::endl;
         } else {
             std::cout << line << std::endl;
         }
